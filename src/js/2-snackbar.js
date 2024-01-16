805c39ab1c750ec94b1252c2212cfcadc1467908
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-01-16 15:40:48 +0200

    main

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,50 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stateInput = document.querySelector('input[name="state"]:checked');
+
+  const delay = parseInt(delayInput.value, 10);
+  const state = stateInput ? stateInput.value : null;
+
+  if (isNaN(delay) || delay < 0) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a valid delay value',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else if (state === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise.then(
+    (delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    },
+    (delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    }
+  );
+});
